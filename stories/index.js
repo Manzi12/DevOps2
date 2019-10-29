306013ee8f7333bbbb2e6cac0f5c150328cf9347
Manzi Joseph <manzeyjoseph@gmail.com>
2019-10-29 12:03:17 +0000

    Header component completed

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
+import Header from '../src/components/header/'
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+storiesOf("Contact List App/Header",module).add("default",() => (
+    <Header noContacts = {10} />
+));
