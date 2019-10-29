59ee6aa0118227984beeac08754502acc26ad1e3
Manzi Joseph <manzeyjoseph@gmail.com>
2019-10-29 21:22:00 +0000

    Contact component completed

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import Header from '../src/components/header/'
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import FilterControls from '../public/components/filterControls';
+import Contact from '../src/components/contact';
 
 storiesOf("Contact List App/Header",module).add("default",() => (
     <Header noContacts = {10} />
@@ -11,4 +12,15 @@ storiesOf("Contact List App/Header",module).add("default",() => (
 
 storiesOf("Contact List App/Filter Controls",module).add("default",()=>(
     <FilterControls/>
+));
+
+const sample ={
+    name: {first:'Joe',last:'Bloggs'},
+    email:'j.bloggs@example.com',
+    phone:'012-23456789',
+    picture:{thumbnail:'./profile.png'}
+}
+
+storiesOf("Contact List App/Contact",module).add("default",()=>(
+    <Contact contact = {sample}/>
 ));
