18297d79f9154f20df3624c15cee324576103460
Manzi Joseph <manzeyjoseph@gmail.com>
2019-11-20 19:55:18 +0000

    Stub contact page working

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React, { Component } from "react";
+import React, { Component,Fragment } from "react";
 import Header from "./components/header/";
 import ContactList from "./components/contactList/";
 import FilterControls from "./components/filterControls/";
 import request from "superagent";
 import api from "./dataStore/stubAPI"; // NEW
+import _ from "lodash";
 
 class App extends Component {
   state = { search: "", gender: "all" };
-    componentDidMount() {
-        request.get("https://randomuser.me/api/?results=50").end((error, res) => {
-        if (res) {
-            let { results: contacts } = JSON.parse(res.text);
-            api.initialize(contacts);
-            this.setState({});
-        } else {
-            console.log(error);
-        }
-        });
-    }
 
     deleteContact = (key) => {
       api.delete(key); 
       this.setState({});                          
   };
 
+  handleChange = (type, value) => {
+    type === "name"
+    ? this.setState({ search: value })
+    : this.setState({ gender: value });
+};
+
     render() {
         let contacts = api.getAll();
-        return (
-        <div className="jumbotron">
-            <Header noContacts={contacts.length} />
-            <FilterControls />
-            <ContactList contacts={contacts} />
-            deleteHandler={this.deleteContact} />
-        </div>
-        );
+        let filteredContacts = contacts.filter(c => {
+            const name = `${c.name.first} ${c.name.last}`;
+            return name.toLowerCase().search(this.state.search.toLowerCase()) !== -1;
+            });
+            filteredContacts =
+            this.state.gender === "all"
+                ? filteredContacts
+                : filteredContacts.filter(c => c.gender === this.state.gender);
+            let sortedContacts = _.sortBy(filteredContacts, c => c.name.last);
+            return (
+                <Fragment>
+                  <Header noContacts={sortedContacts.length} />
+                  <FilterControls onUserInput={this.handleChange} />
+                  <ContactList
+                    contacts={sortedContacts}
+                    deleteHandler={this.deleteContact}
+                  />
+                </Fragment>
+              );
     }
 }
 
--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,7 +1,12 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+//import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
-library.add(
+import {
     faEnvelope,
-    faPhone
-);
+    faPhone,
+    faHome,
+    faBirthdayCake,
+    faArrowCircleLeft
+  } from "@fortawesome/free-solid-svg-icons";
+  
+  library.add(faEnvelope, faPhone, faHome, faBirthdayCake, faArrowCircleLeft);
